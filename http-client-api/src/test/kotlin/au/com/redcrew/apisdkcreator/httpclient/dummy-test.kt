373c60df1cd8ba82c4bac653b0a200cb63828fc0
Kieran Simpson <kieran@redcrew.com.au>
2021-05-31 15:37:37 +1000

    Add dummy test

---
+++ http-client-api/src/test/kotlin/au/com/redcrew/apisdkcreator/httpclient/dummy-test.kt
@@ -0,0 +1,12 @@
+package au.com.redcrew.apisdkcreator.httpclient
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.jupiter.api.Test
+
+class DummyTest {
+    @Test
+    fun `should expect true`() {
+        assertThat(true, equalTo(true))
+    }
+}
