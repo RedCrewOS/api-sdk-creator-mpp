3e1933022b7c08621acdce23dfa7ea3297dcdfcf
Kieran Simpson <kieran@redcrew.com.au>
2021-06-09 16:44:33 +1000

    Experiment with using named functions

--- http-client-api/src/main/kotlin/au/com/redcrew/apisdkcreator/httpclient/maths.kt
+++ http-client-api/src/main/kotlin/au/com/redcrew/apisdkcreator/httpclient/maths.kt
@@ -3,11 +3,14 @@ package au.com.redcrew.apisdkcreator.httpclient
 import arrow.core.compose
 import arrow.core.curried
 
-val add = { a: Int, b: Int -> a + b }
-val multiply = { a: Int, b: Int -> a * b }
+fun add(a: Int, b: Int) = a + b
+fun multiply(a: Int, b: Int) = a * b
 
-val add3 = add.curried()(3)
-val multiply2 = multiply.curried()(2)
+/*val add = { a: Int, b: Int -> a + b }
+val multiply = { a: Int, b: Int -> a * b }*/
+
+val add3 = ::add.curried()(3)
+val multiply2 = ::multiply.curried()(2)
 
 val m2a3 = add3 compose multiply2
 
