4559d6d73e9782eb7a76c486fad7d287d828dbd7
Kieran Simpson <kieran@redcrew.com.au>
2021-06-09 16:34:57 +1000

    Experiment with suspending functions

--- http-client-api/src/main/kotlin/au/com/redcrew/apisdkcreator/httpclient/maths.kt
+++ http-client-api/src/main/kotlin/au/com/redcrew/apisdkcreator/httpclient/maths.kt
@@ -11,3 +11,12 @@ val multiply2 = multiply.curried()(2)
 
 val m2a3 = add3 compose multiply2
 
+val susAdd: suspend (Int, Int) -> Int = { a, b -> add(a, b) }
+val susAdd3 = susAdd.curried()(3)
+
+val susMultiply: suspend (Int, Int) -> Int = { a, b -> multiply(a, b) }
+val susMultiply2 = susMultiply.curried()(2)
+
+val susM2A3: suspend (Int) -> Int = { a ->
+    susAdd3(susMultiply2(a))
+}
--- http-client-api/src/test/kotlin/au/com/redcrew/apisdkcreator/httpclient/maths-test.kt
+++ http-client-api/src/test/kotlin/au/com/redcrew/apisdkcreator/httpclient/maths-test.kt
@@ -2,7 +2,12 @@ package au.com.redcrew.apisdkcreator.httpclient
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+
 
 class MathsTest {
     @Test
@@ -18,5 +23,27 @@ class MathsTest {
     @Test
     fun `should multiply by two then add three`() {
         assertThat(m2a3(2), equalTo(7))
+    }
+
+    // see https://craigrussell.io/2019/11/unit-testing-coroutine-suspend-functions-using-testcoroutinedispatcher/
+    @ExperimentalCoroutinesApi
+    @Nested
+    inner class SuspendingMathsTest {
+        private val testDispatcher = TestCoroutineDispatcher()
+
+        @Test
+        fun `should add two numbers`() = testDispatcher.runBlockingTest {
+            assertThat(susAdd(2, 3), equalTo(5))
+        }
+
+        @Test
+        fun `should multiply two numbers`() = testDispatcher.runBlockingTest {
+            assertThat(susMultiply(5, 3), equalTo(15))
+        }
+
+        @Test
+        fun `should multiply by two then add three`() = testDispatcher.runBlockingTest {
+            assertThat(susM2A3(2), equalTo(7))
+        }
     }
 }
