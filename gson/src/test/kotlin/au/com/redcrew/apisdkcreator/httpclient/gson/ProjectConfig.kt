ccd34ca9ea3cc5286ccfb08964609db70b1eba76
Kieran Simpson <kieran@redcrew.com.au>
2022-02-10 12:42:47 +1100

    Move JVM modules to JVM repo.

--- gson/src/test/kotlin/au/com/redcrew/apisdkcreator/httpclient/gson/ProjectConfig.kt
+++
@@ -1,9 +0,0 @@
-package au.com.redcrew.apisdkcreator.httpclient.gson
-
-import io.kotest.common.ExperimentalKotest
-import io.kotest.core.config.AbstractProjectConfig
-
-@ExperimentalKotest
-class ProjectConfig : AbstractProjectConfig() {
-    override var testCoroutineDispatcher = true
-}
