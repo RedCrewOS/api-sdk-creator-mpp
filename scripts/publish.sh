23250fb1c710b064f61e4400caccda6b68c963df
Kieran Simpson <kieran@redcrew.com.au>
2022-04-19 12:10:18 +1000

    Fix task to call to publish.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -22,7 +22,7 @@ if git show-ref --tags $tag --quiet; then
   echo "Tag exists"
 else
   echo "Publish to sonatype"
-  ./gradlew publishToSonatype closeAndReleaseSonatypeStagingRepository
+  ./gradlew publish closeAndReleaseSonatypeStagingRepository
 
   echo "Tagging with ${tag}"
   git tag $tag
